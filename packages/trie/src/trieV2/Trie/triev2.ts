302d617fc80a7377fadb8bcb35d5fe37a2a6802c
ScottyPoi <scott.simpson@ethereum.org>
2023-04-29 02:07:13 -0600

    Trie: create new Trie class

--- packages/trie/src/trieV2/Trie/triev2.ts
+++ packages/trie/src/trieV2/Trie/triev2.ts
@@ -1,11 +1,20 @@
-import type { LRUCache } from 'lru-cache'
+import { Mutex } from 'async-mutex'
+import debug from 'debug'
+import { keccak256 } from 'ethereum-cryptography/keccak'
+import { LRUCache } from 'lru-cache'
+
+import type { HashFunction, Node } from '../Node'
+import type { Debugger } from 'debug'
 
 export type Nibble = number
 export interface WalkOptions {}
 
 export interface TrieInterface {
-  _operationLock: any
+  _operationLock: Mutex
+  _hashFunction: HashFunction
+  readonly _checkpoints: Uint8Array[]
   cache: LRUCache<string, Node>
+  _root: Node | null
   root: Uint8Array
   get(key: Uint8Array): Promise<Uint8Array | null>
   put(key: Uint8Array, value: Uint8Array): Promise<void>
@@ -34,4 +43,32 @@ export interface TrieInterface {
   withLock<T>(operation: () => Promise<T>): Promise<T>
   garbageCollect(): Promise<void>
   _markReachableNodes(node: Node | null, reachableHashes: Set<string>): Promise<void>
+}
+
+export type TrieOpts = {
+  _root?: Node
+  root?: Uint8Array
+  cache?: LRUCache<string, Node>
+  _checkpoints?: Uint8Array[]
+  _hashFunction?: HashFunction
+}
+
+export class Trie implements TrieInterface {
+  _root: Node | null
+  root: Uint8Array
+  private readonly _hashFunction: HashFunction
+  private readonly _checkpoints: Uint8Array[]
+  private readonly _operationLock: any
+  private readonly cache: LRUCache<string, Node>
+  private readonly debug: Debugger
+
+  constructor(opts: TrieOpts = {}) {
+    this._root = opts._root ?? null
+    this.root = opts.root ?? Uint8Array.from([0x80])
+    this._checkpoints = []
+    this._operationLock = new Mutex()
+    this._hashFunction = opts._hashFunction ?? keccak256
+    this.cache = new LRUCache({ max: 1000 })
+    this.debug = debug('TRIE')
+  }
 }
