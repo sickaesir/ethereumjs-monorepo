1e11897fcd7cf71d69da3e835f90d7f675873a15
ScottyPoi <scott.simpson@ethereum.org>
2023-05-03 21:03:36 -0600

    define a NullNode class -- update Base

--- packages/trie/src/trieV2/Node/baseNode.ts
+++ packages/trie/src/trieV2/Node/baseNode.ts
@@ -1,15 +1,52 @@
 import debug from 'debug'
 import { keccak256 } from 'ethereum-cryptography/keccak'
 
-import type { HashFunction } from '../types'
+import { decodeNibbles } from '../util'
+
+import { TrieNode } from './trieNode'
+
+import type { HashFunction, Nibble, NodeType, TNode } from '../types'
 import type { Debugger } from 'debug'
 
 export abstract class BaseNode {
+  public type: NodeType
   debug: Debugger
   hashFunction: HashFunction
   constructor(args: any) {
+    this.type = 'NullNode'
     this.debug = debug(this.constructor.name)
     this.hashFunction = args.hashFunction ?? keccak256
     this.debug('hey ')
+  }
+  abstract get(rawKey?: Uint8Array): Promise<Uint8Array | null>
+  abstract rlpEncode(): Uint8Array
+  abstract update(rawKey: Uint8Array, value: Uint8Array): Promise<TNode>
+  abstract getChildren(): Promise<Map<number, TNode>>
+  abstract getPartialKey(): Nibble[]
+}
+
+export class NullNode extends BaseNode {
+  type: 'NullNode' = 'NullNode'
+  constructor() {
+    super({})
+  }
+  rlpEncode(): Uint8Array {
+    return Uint8Array.from([])
+  }
+  hash(): Uint8Array {
+    return this.hashFunction(this.rlpEncode())
+  }
+  async get(): Promise<Uint8Array | null> {
+    return null
+  }
+  async getChildren(): Promise<Map<number, TNode>> {
+    return new Map()
+  }
+  getPartialKey(): Nibble[] {
+    return []
+  }
+  async update(key: Uint8Array, value: Uint8Array): Promise<TNode> {
+    const newNode = await TrieNode.create({ key: decodeNibbles(key), value })
+    return newNode
   }
 }
