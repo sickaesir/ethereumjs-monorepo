304f5946f6c2fae7fbb8242eb2c7c13ca4cf627c
ScottyPoi <scott.simpson@ethereum.org>
2023-05-05 03:20:56 -0600

    create scaffold for MerklePatriciaTrie class

--- packages/trie/src/trieV2/Trie/trie.ts
+++ packages/trie/src/trieV2/Trie/trie.ts
@@ -1,1 +1,23 @@
-export class Trie {}
+import type { TrieInterface } from './trieInterface'
+
+export interface CreateTrieOptions {}
+export class MerklePatriciaTrie implements TrieInterface {
+  static async create(_options: CreateTrieOptions): Promise<MerklePatriciaTrie> {
+    const trie = await MerklePatriciaTrie.create({})
+    return trie
+  }
+  static async fromProof(_proof: Uint8Array[]): Promise<MerklePatriciaTrie> {
+    const trie = await MerklePatriciaTrie.create({})
+    return trie
+  }
+  static async fromMultiProof(_proof: Uint8Array[]): Promise<MerklePatriciaTrie> {
+    const trie = await MerklePatriciaTrie.create({})
+    return trie
+  }
+  static async verifyProof(_proof: Uint8Array[]): Promise<boolean> {
+    return true
+  }
+  static async verifyMultiProof(_proof: Uint8Array[]): Promise<boolean> {
+    return true
+  }
+}
