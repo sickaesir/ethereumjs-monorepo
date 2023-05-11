24e6a56601cb49922ba33075a17907478134b258
ScottyPoi <scott.simpson@ethereum.org>
2023-05-10 20:57:07 -0600

    update extension node

--- packages/trie/src/trieV2/Node/extensionNode.ts
+++ packages/trie/src/trieV2/Node/extensionNode.ts
@@ -1,95 +1,98 @@
 import { RLP } from '@ethereumjs/rlp'
+import { equalsBytes } from '@ethereumjs/util'
 import { keccak256 } from 'ethereum-cryptography/keccak'
 
 import {
-  addPadding,
   concatNibbles,
   decodeNibbles,
   encodeNibbles,
   hasMatchingNibbles,
   matchingNibbleLength,
-  nibblesEqual,
-  unPad,
+  nibblesToKey,
 } from '../index'
 
 import { BaseNode, BranchNode, LeafNode, NullNode } from './index'
 
-import type { Nibble, NodeInterface, TNode, TNodeOptions } from '../index'
+import type { Nibble, NodeInterface, NodeType, TNode, TNodeOptions } from '../index'
 
 export class ExtensionNode extends BaseNode implements NodeInterface<'ExtensionNode'> {
   type = 'ExtensionNode' as const
   keyNibbles: Nibble[]
-  // key: Uint8Array
   child: TNode
 
   constructor(options: TNodeOptions<'ExtensionNode'>) {
     super(options)
-    this.keyNibbles = unPad(options.keyNibbles)
+    this.keyNibbles = options.keyNibbles
     this.child = options.subNode
     this.debug &&
-      this.debug(
-        `ExtensionNode({ keyNibbles ${options.keyNibbles}, child }) created with keyNibbles=${
-          this.keyNibbles
-        }, key=${' '}, childNibbles=${this.child.getPartialKey()}`
-      )
+      this.debug(`Created with keyNibbles=${this.keyNibbles}, child=${this.child.getType()}`)
   }
 
   rlpEncode(): Uint8Array {
-    this.debug &&
-      this.debug(
-        `ExtensionNode rlpEncode: keyNibbles=${this.keyNibbles} + child=${this.child.hash()}`
-      )
-    const encodedNode = RLP.encode([
-      encodeNibbles(addPadding(this.keyNibbles)),
-      this.child.rlpEncode(),
-    ])
-    this.debug && this.debug(`ExtensionNode encoded: ${encodedNode}`)
+    const encodedNode = RLP.encode([nibblesToKey(this.keyNibbles), this.child.rlpEncode()])
     return encodedNode
   }
 
   hash(): Uint8Array {
     const encodedNode = this.rlpEncode()
     const hashed = keccak256(encodedNode)
-    this.debug && this.debug(`ExtensionNode hash: ${hashed}`)
     return hashed
   }
 
-  async getChildren(): Promise<Map<number, TNode>> {
+  getChildren(): Map<number, TNode> {
     return new Map().set(0, this.child)
+  }
+  getChild(_key: number = 0): TNode {
+    return this.child
+  }
+  getType(): NodeType {
+    return 'ExtensionNode'
+  }
+  async deleteChild(_nibble: number): Promise<TNode> {
+    return new NullNode()
+  }
+  updateChild(newNode: TNode): TNode {
+    if (equalsBytes(newNode.hash(), this.child.hash())) {
+      return this
+    }
+    const newKeyNibbles = this.keyNibbles.slice(1)
+    if (newKeyNibbles.length === 0) {
+      return newNode
+    }
+    const updatedNode = new ExtensionNode({ keyNibbles: newKeyNibbles, subNode: newNode })
+    return updatedNode
+  }
+  async updateValue(_newValue: Uint8Array): Promise<TNode> {
+    return this
+  }
+  updateKey(_newKey: Nibble[]): TNode {
+    this.keyNibbles = _newKey
+    return this
+  }
+  getValue(): Uint8Array | undefined {
+    return undefined
   }
   getPartialKey(): Nibble[] {
     return this.keyNibbles
   }
-  async get(rawKey: Uint8Array): Promise<Uint8Array | null> {
-    const key = decodeNibbles(rawKey)
-
-    if (nibblesEqual(key.slice(0, this.keyNibbles.length), this.keyNibbles)) {
-      this.debug &&
-        this.debug(
-          ` key shares the same prefix as the existing key, get the child: ${this.child.getPartialKey()}`
-        )
-      const result = await this.child.get(encodeNibbles(key.slice(this.keyNibbles.length)))
-      this.debug && this.debug(`ExtensionNode get result: ${result === null ? 'null' : result}`)
-      return result
-    }
-    this.debug && this.debug(`ExtensionNode get result: null`)
-    return null
+  async get(_rawKey: Uint8Array): Promise<Uint8Array | null> {
+    throw new Error('method to be deleted')
   }
 
-  async update(rawKey: Uint8Array, value: Uint8Array): Promise<TNode> {
-    this.debug && this.debug(`ExtensionNode update: rawKey=${rawKey}, value=${value}`)
-    const key = decodeNibbles(rawKey)
+  async update(value: Uint8Array): Promise<TNode> {
+    this.debug && this.debug.extend('update')(`value=${value}`)
+    const key = this.getPartialKey()
     const matching = matchingNibbleLength(this.keyNibbles, key)
 
     // If the entire key matches
     if (matching === this.keyNibbles.length && matching === key.length) {
-      const updatedChild = await this.child.update(new Uint8Array(), value)
+      const updatedChild = await this.child.update(value)
       return new ExtensionNode({ keyNibbles: this.keyNibbles, subNode: updatedChild })
     }
 
     // If there's a partial match
     if (matching > 0 && matching < this.keyNibbles.length) {
-      const updatedChild = await this.child.update(encodeNibbles(key.slice(matching)), value)
+      const updatedChild = await this.child.update(value)
       const newKeyNibbles = this.keyNibbles.slice(0, matching)
       const branch = new BranchNode({
         children: [
@@ -126,7 +129,7 @@ export class ExtensionNode extends BaseNode implements NodeInterface<'ExtensionN
 
     // If the extension key is a prefix of the new key
     if (matching === this.keyNibbles.length) {
-      const updatedChild = await this.child.update(encodeNibbles(key.slice(matching)), value)
+      const updatedChild = await this.child.update(value)
       return new ExtensionNode({ keyNibbles: this.keyNibbles, subNode: updatedChild })
     }
 
@@ -136,15 +139,16 @@ export class ExtensionNode extends BaseNode implements NodeInterface<'ExtensionN
 
   async delete(rawKey: Uint8Array): Promise<ExtensionNode | NullNode> {
     const key = decodeNibbles(rawKey)
-    this.debug && this.debug(`ExtensionNode delete: rawKey:${rawKey}...decoded: ${key}`)
-    this.debug && this.debug(`ExtensionNode: ${this.keyNibbles}`)
+    this.debug && this.debug.extend('delete')(`[${key}]`)
 
     if (!hasMatchingNibbles(this.keyNibbles, key)) {
+      this.debug && this.debug.extend('delete')(`key does not match`)
       // The key does not match the extension node, return the original node
       return this
     }
 
     const remainingKey = key.slice(this.keyNibbles.length)
+    this.debug && this.debug.extend('delete')(`remainingKey=${remainingKey}`)
     const updatedChild = await this.child.delete(encodeNibbles(remainingKey))
 
     if (updatedChild instanceof NullNode) {
