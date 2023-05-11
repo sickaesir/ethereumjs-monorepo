bf853b40f864ed6f9f022421de4cba9de50bb45f
ScottyPoi <scott.simpson@ethereum.org>
2023-05-10 20:39:06 -0600

    update leafnode

--- packages/trie/src/trieV2/Node/leafNode.ts
+++ packages/trie/src/trieV2/Node/leafNode.ts
@@ -2,61 +2,66 @@ import { RLP } from '@ethereumjs/rlp'
 import { keccak256 } from 'ethereum-cryptography/keccak'
 import { equalsBytes } from 'ethereum-cryptography/utils'
 
-import { decodeNibbles, encodeNibbles, nibblesEqual } from '../util'
+import { decodeNibbles, nibblesEqual, nibblesToKey } from '../util'
 
 import { BaseNode, NullNode } from './index'
 
-import type { Nibble, NodeInterface, TNode, TNodeOptions } from '../types'
+import type { Nibble, NodeInterface, NodeType, TNode, TNodeOptions } from '../types'
 
 export class LeafNode extends BaseNode implements NodeInterface<'LeafNode'> {
   type = 'LeafNode' as const
   key: Uint8Array
   keyNibbles: Nibble[]
   value: Uint8Array | null
-
   constructor(options: TNodeOptions<'LeafNode'>) {
     super(options)
-    this.key = encodeNibbles(options.key)
+    this.key = nibblesToKey(options.key)
     this.keyNibbles = options.key
     this.value = options.value
-    this.debug && this.debug(`LeafNode created: key=${options.key}, value=${options.value}`)
+    this.debug && this.debug(`Created with keyNibbles: ${this.keyNibbles}`)
   }
-
   rlpEncode(): Uint8Array {
-    this.debug && this.debug(`LeafNode encode: key=${this.key}, value=${this.value}`)
-    const encodedNode = RLP.encode([this.key, this.value])
-    this.debug && this.debug(`LeafNode encoded: ${encodedNode}`)
+    const encodedNode = RLP.encode([Uint8Array.from(this.keyNibbles), this.value])
     return encodedNode
   }
-
   hash(): Uint8Array {
-    const encodedNode = this.rlpEncode()
-    const hashed = keccak256(encodedNode)
-    this.debug && this.debug(`LeafNode hash: ${hashed}`)
+    const hashed = keccak256(this.rlpEncode())
     return hashed
   }
-
   async get(rawKey: Uint8Array): Promise<Uint8Array | null> {
-    this.debug && this.debug(`LeafNode get: rawKey=${rawKey}`)
-    this.debug && this.debug(`LeafNode: key=${this.key}, keyNibbles: ${this.keyNibbles}`)
     const result = equalsBytes(this.key, rawKey) ? this.value : null
-    this.debug && this.debug(`LeafNode get result: ${result ? result : 'null'}`)
     return result
   }
-
-  async getChildren(): Promise<Map<number, TNode>> {
+  getChildren(): Map<number, TNode> {
     return new Map()
+  }
+  getChild(_key: number): TNode {
+    throw new Error('LeafNode does not have children')
+  }
+  getValue(): Uint8Array | undefined {
+    return this.value ?? undefined
   }
   getPartialKey(): Nibble[] {
     return this.keyNibbles
   }
-  async update(rawKey: Uint8Array, value: Uint8Array): Promise<LeafNode> {
-    return new LeafNode({ key: decodeNibbles(rawKey), value })
+  getType(): NodeType {
+    return 'LeafNode'
+  }
+  async update(value: Uint8Array | null): Promise<LeafNode> {
+    return new LeafNode({ key: this.keyNibbles, value })
+  }
+  async updateValue(value: Uint8Array | null): Promise<LeafNode> {
+    return new LeafNode({ key: this.keyNibbles, value })
+  }
+  updateChild(_node: TNode = new NullNode()): TNode {
+    throw new Error('LeafNode does not have children')
+  }
+  async deleteChild(_nibble: Nibble): Promise<TNode> {
+    return this
   }
   updateKey(newKeyNibbles: Nibble[]): LeafNode {
     return new LeafNode({ key: newKeyNibbles, value: this.value })
   }
-
   async delete(rawKey: Uint8Array): Promise<TNode> {
     const key = decodeNibbles(rawKey)
     if (nibblesEqual(this.getPartialKey(), key)) {
